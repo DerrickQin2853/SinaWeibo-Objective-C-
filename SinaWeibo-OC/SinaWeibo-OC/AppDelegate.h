e83ee27e5cb73bd201bd8de1777acdb377d5d148
DerrickQin <si_yuan_qin@163.com>
2016-09-22 21:56:45 +0800

    初始化项目

---
+++ SinaWeibo-OC/SinaWeibo-OC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SinaWeibo-OC
+//
+//  Created by admin on 16/9/22.
+//  Copyright © 2016年 Derrick_Qin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
